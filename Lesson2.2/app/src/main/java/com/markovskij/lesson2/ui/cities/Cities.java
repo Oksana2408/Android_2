30df451c324d53a8a94449ef86a35c073b74ca41
Oksana2408 <oksanamk@gmx.de>
2020-04-16 10:27:47 +0200

    ДЗ ко 2 уроку

---
+++ Lesson2.2/app/src/main/java/com/markovskij/lesson2/ui/cities/Cities.java
@@ -0,0 +1,13 @@
+package com.markovskij.lesson2.ui.cities;
+
+public class Cities {
+    private String name;
+
+    public Cities(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
